4790e52cc3ccd2681def0bfeda9afe6abc772cac
filip2cz <filda.komarek@seznam.cz>
2022-11-13 01:57:06 +0100

    number mode

--- start.sh
+++ start.sh
@@ -6,8 +6,15 @@ if [ "$ui" = "update" ]; then
     sudo rm -r mccivilization-adminui-ssh && echo "deleted old ver" && git clone https://github.com/filip2cz/mccivilization-adminui-ssh.git && sudo chmod 777 ./mccivilization-adminui-ssh/* && exec ./mccivilization-adminui-ssh/start.sh
 fi
 if [ "$ui" = "number" ]; then
-    echo "not implemented yet"
-    else
+    echo [1] Starting server
+    echo [2] Stop server
+    echo [3] Restart server
+    echo [4] Server status
+    echo [5] Edit server and plugins config
+    echo [6] Server console
+    echo [7] Show this message
+    echo [0] Exit
+else
     echo [start] Starting server
     echo [stop] Stop server
     echo [restart] Restart server
@@ -16,61 +23,74 @@ if [ "$ui" = "number" ]; then
     echo [console] Server console
     echo [help] Show this message
     echo [exit]/[quit] Exit
-    while :
-    do
-        read -p '> ' command
-        case $command in
-            "exit")
-                exit
-                ;;
-            "quit")
-                exit
-                ;;
-            "help")
+fi
+while :
+do
+    read -p '> ' command
+    case $command in
+        "exit")
+            exit
+        ;;
+        "quit")
+            exit
+        ;;
+        "help")
+            if [ "$uid" = "number" ]; then
+                echo [1] Starting server
+                echo [2] Stop server
+                echo [3] Restart server
+                echo [4] Server status
+                echo [5] Edit server and plugins config
+                echo [6] Server console
+                echo [7] Show this message
+                echo [0] Exit
+            else
                 echo [start] Starting server
                 echo [stop] Stop server
                 echo [restart] Restart server
                 echo [status] Server status
+                echo [config] Edit server and plugins config
+                echo [console] Server console
                 echo [help] Show this message
                 echo [exit]/[quit] Exit
-                ;;
-            "start")
-                sudo systemctl start mccivilization && echo "Server started"
-                ;;
-            "stop")
-                sudo systemctl stop mccivilization && echo "Server stopped"
-                ;;
-            "status")
-                sudo systemctl status mccivilization
-                ;;
-            "restart")
-                echo "Restarting server..." && sudo systemctl stop mccivilization && sleep 30 && sudo systemctl start mccivilization && echo "Server restarted"
-                ;;
-            "update")
-                sudo rm -r mccivilization-adminui-ssh && echo "deleted old ver" && git clone https://github.com/filip2cz/mccivilization-adminui-ssh.git && sudo chmod 777 ./mccivilization-adminui-ssh/* && exec ./mccivilization-adminui-ssh/start.sh
-                ;;
-            "pwd")
-                pwd
-                ;;
-            "uname")
-                uname -a
-                ;;
-            "console")
-                /usr/local/bin/mcrcon -H 127.0.0.1 -P 20656 -p filipjebuh
-                ;;
-            "config")
-                echo "Plugins: essentialsx"
-                echo "or entet cancel to cancel"
-                read -p 'what plugin?: [essentialsx] ' plugin
-                if [ "$plugin" = "essentialsx" ]; then
-                    sudo nano /home/filip/mccivilization/plugins/Essentials/config.yml && sudo chmod 777 /home/filip/mccivilization/plugins/Essentials/config.yml
-                else
-                    sudo nano /home/filip/mccivilization/plugins/Essentials/config.yml && sudo chmod 777 /home/filip/mccivilization/plugins/Essentials/config.yml
-                fi
-                ;;
-            *)
-                echo "Unknown command"
-                ;;
-        esac
-    done
-fi
+            fi
+        ;;
+        "start" | "1")
+            sudo systemctl start mccivilization && echo "Server started"
+        ;;
+        "stop" | "2")
+            sudo systemctl stop mccivilization && echo "Server stopped"
+        ;;
+        "status" | "4")
+            sudo systemctl status mccivilization
+        ;;
+        "restart" | "3")
+            echo "Restarting server..." && sudo systemctl stop mccivilization && sleep 30 && sudo systemctl start mccivilization && echo "Server restarted"
+        ;;
+        "update")
+            sudo rm -r mccivilization-adminui-ssh && echo "deleted old ver" && git clone https://github.com/filip2cz/mccivilization-adminui-ssh.git && sudo chmod 777 ./mccivilization-adminui-ssh/* && exec ./mccivilization-adminui-ssh/start.sh
+        ;;
+        "pwd")
+            pwd
+        ;;
+        "uname")
+            uname -a
+        ;;
+        "console" | "6")
+            /usr/local/bin/mcrcon -H 127.0.0.1 -P 20656 -p filipjebuh
+        ;;
+        "config" | "5")
+            echo "Plugins: essentialsx"
+            echo "or entet cancel to cancel"
+            read -p 'what plugin?: [essentialsx] ' plugin
+            if [ "$plugin" = "essentialsx" ]; then
+                sudo nano /home/filip/mccivilization/plugins/Essentials/config.yml && sudo chmod 777 /home/filip/mccivilization/plugins/Essentials/config.yml
+            else
+                sudo nano /home/filip/mccivilization/plugins/Essentials/config.yml && sudo chmod 777 /home/filip/mccivilization/plugins/Essentials/config.yml
+            fi
+        ;;
+        *)
+            echo "Unknown command"
+        ;;
+    esac
+done
