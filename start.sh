4392612e558df5065a7efeca4c3e39dc0f51cc48
filip2cz <filip@fkomarek.eu>
2022-11-14 13:35:36 +0100

    s

--- start.sh
+++ start.sh
@@ -113,57 +113,7 @@ do
             /usr/local/bin/mcrcon -H 127.0.0.1 -P 20656 -p filipjebuh
         ;;
         "config" | "6")
-            if [ "$ui" = "number" ]; then
-                clear
-            fi
-            echo "Server configs:"
-            echo "Empty"
-            echo "Plugins:"
-            echo [1] EssentialsX
-            echo [2] Multiverse-Core
-            echo [3] Multiverse-
-            echo [4] Multiverse-
-            echo "Other options:"
-            echo [0] Cancel
-            read -p 'what config to edit? ' config
-            if [ "$config" = "1" ]; then
-                sudo nano /home/filip/mccivilization/plugins/Essentials/config.yml
-            elif [ "$config" = "2" ]; then
-                if [ "$ui" = "number" ]; then
-                    clear
-                else
-                    echo
-                fi
-                echo [1] "./Multiverse-Core/config.yml"
-                echo [2] "./Multiverse-Core/worlds.yml"
-                echo [0] "cancel"
-                read -p "which file? " configFile
-                if [ "$configFile" = "1" ]
-                    sudo nano /home/filip/mccivilization/plugins/Multiverse-Core/config.yml
-                else if [ "$configFile" = "2" ]
-                    sudo nano /home/filip/mccivilization/plugins/Multiverse-Core/worlds.yml
-                elif [ "$configFile" = "0" ]
-                    echo "canceled"
-                else
-                    echo "invalid option, exiting"
-                    if [ "$ui" = "number" ]; then
-                        read -p 'Press ENTER to close this message' $null
-                    fi
-                fi
-            elif [ "$config" = "3" ]; then
-                sudo nano /home/filip/mccivilization/plugins/Multiverse-Core/config.yml
-            
-            elif [ "$config" = "4" ]; then
-                sudo nano /home/filip/mccivilization/plugins/Multiverse-Core/config.yml
-            
-            elif [ "$config" = "0" ]; then
-                echo "canceled"
-            else
-                echo "Invalid option, canceling..."
-                if [ "$ui" = "number" ]; then
-                    read -p 'Press ENTER to close this message' $null
-                fi
-            fi
+            sh ./mccivilization-adminui-ssh/serverconfig.sh
         ;;
         "plugins" | "7")
             if [ "$ui" = "number" ]; then
